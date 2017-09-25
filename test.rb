a1559c930087dbbee6ec13c24c633ae7432d9504
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-09-24 19:25:10 -0500

    Build Worker API

--- test.rb
+++ test.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
-require 'open-uri'
-
-desc = RUBY_DESCRIPTION.ljust(60)
-begin
-  temp = OpenURI.open_uri('https://raw.githubusercontent.com/gcc-mirror/gcc/master/config.guess')
-  puts "#{desc} SSL Verified"
-rescue => e
-  puts "#{desc} SSL Failed\n   #{e.message}"
-end
+`appveyor PushArtifact test_OpenURI.rb -filename \"Do Spaces Work?\"`
+exit 1
