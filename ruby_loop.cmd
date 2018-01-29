35e06b774463bb10b15566874071148f86522542
MSP-Greg <MSP-Greg@users.noreply.github.com>
2018-01-29 17:11:58 -0600

    gem info

--- ruby_loop.cmd
+++ ruby_loop.cmd
@@ -1,15 +1,14 @@
 @echo off
 
 if "%APPVEYOR%"=="True" (
-  echo Running in APPVEYOR
   set ruby_pre=C:
-  set vers=ruby200 ruby200-x64 ruby21 ruby21-x64 ruby22 ruby22-x64 ruby23 ruby23-x64 ruby24 ruby24-x64 ruby_trunk
+  set vers=ruby200 ruby200-x64 ruby21 ruby21-x64 ruby22 ruby22-x64 ruby23 ruby23-x64 ruby24 ruby24-x64 ruby25-x64 ruby_trunk
 ) else (
-  echo NOT RUNNING IN APPVEYOR
-  set base_path=C:/Windows/system32;C:/Windows;C:/Windows/System32/Wbem;C:/Windows/System32/WindowsPowerShell/v1.0;C:/ruby25_64/bin;E:/msys64;C:/Program Files/Microsoft SQL Server/130/Tools/Binn;C:/Program Files/Git/cmd;"C:/Program Files (x86)/GNU/GnuPG/bin"
   set ruby_pre=E:/r_builds_RI2
   set vers=ruby200 ruby200-x64 ruby21 ruby21-x64 ruby22 ruby22-x64 ruby23 ruby23-x64 ruby24 ruby24-x64 ruby_trunk
+  set base_path=C:/Windows/system32;C:/Windows;C:/Windows/System32/Wbem;C:/Windows/System32/WindowsPowerShell/v1.0;C:/ruby25_64/bin;E:/msys64;C:/Program Files/Microsoft SQL Server/130/Tools/Binn;C:/Program Files/Git/cmd;"C:/Program Files (x86)/GNU/GnuPG/bin"
 )
+
 @echo ————————————————————————————————————————————————————————————————— %1
 @echo.
 @type %1
@@ -18,6 +17,5 @@ if "%APPVEYOR%"=="True" (
 @for %%A in (%vers%) do (
   PATH=%ruby_pre%/%%A/bin;%base_path%
   call ruby %1 %%A
-  call git --version
 )
 @PATH=%base_path%
